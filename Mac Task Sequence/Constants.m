ed40f8471c4410b4bb2ae38d5322b4f4d8874433
Joseph Rafferty <jrafferty@tamu.edu>
2020-02-01 00:54:24 -0600

    adding sanitized constants definition

---
+++ Mac Task Sequence/Constants.m
@@ -0,0 +1,17 @@
+//
+//  Constants.m
+//  Mosdac
+//
+//  Created by Joseph Rafferty on 5/29/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "Constants.h"
+
+NSString *const DSServerName = @"deploystudioserver";
+NSString *const DSServerPort = @"60080";
+NSString *const DSServerUsername = @"dsadmin";
+NSString *const DSServerPassword = @"password";
+
+NSString *const OSDServerUsername = @"username";
+NSString *const OSDServerPassword = @"password";
